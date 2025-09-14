b4beb20b689f318446c0a947eff04010b78ed819
Kaviya Aravamudhan <kavyaravasri@gmail.com>
2025-09-15 00:05:19 +0530

    outlier part 3

--- convert.py
+++ convert.py
@@ -1,7 +1,24 @@
-
 import pandas as pd
-df = pd.read_csv("KDDTrain+.txt", sep=",")
-df.to_csv("dataset1.csv", index=False)
-df = pd.read_csv("KDDTest+.txt", sep=",")
-df.to_csv("testdataset.csv", index=False)
 
+# NSL-KDD column headers
+headers = [
+    "duration","protocol_type","service","flag","src_bytes","dst_bytes","land",
+    "wrong_fragment","urgent","hot","num_failed_logins","logged_in","num_compromised",
+    "root_shell","su_attempted","num_root","num_file_creations","num_shells",
+    "num_access_files","num_outbound_cmds","is_host_login","is_guest_login",
+    "count","srv_count","serror_rate","srv_serror_rate","rerror_rate","srv_rerror_rate",
+    "same_srv_rate","diff_srv_rate","srv_diff_host_rate","dst_host_count",
+    "dst_host_srv_count","dst_host_same_srv_rate","dst_host_diff_srv_rate",
+    "dst_host_same_src_port_rate","dst_host_srv_diff_host_rate","dst_host_serror_rate",
+    "dst_host_srv_serror_rate","dst_host_rerror_rate","dst_host_srv_rerror_rate",
+    "label","difficulty"
+]
+
+# Read your existing CSV without headers
+df = pd.read_csv("test.csv", header=None)
+
+# Assign headers
+df.columns = headers
+
+# Save CSV with headers
+df.to_csv("test_.csv", index=False)
