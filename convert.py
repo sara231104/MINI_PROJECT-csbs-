6556d0d8fc43065fc037ebcc0531aa0a2e1e954f
Kaviya Aravamudhan <kavyaravasri@gmail.com>
2025-09-14 21:24:44 +0530

    datasets
    Co-authored-by: sara231104 <sara231104@users.noreply.github.com>

--- convert.py
+++ convert.py
@@ -1,4 +1,6 @@
 
 import pandas as pd
 df = pd.read_csv("KDDTrain+.txt", sep=",")
-df.to_csv("dataset.csv", index=False)
+df.to_csv("dataset1.csv", index=False)
+df = pd.read_csv("KDDTest+.txt", sep=",")
+df.to_csv("testdataset.csv", index=False)
